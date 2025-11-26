9faef85fc83c478661e18ed655e2d6a84ad29604
Krish <krishm245@gmail.com>
2025-11-26 11:50:07 +0530

    (fix): potentially fix express vercel deployment

--- backend/api/services/user.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-export const getUsers = (req: VercelRequest, res: VercelResponse) => {
-  res.json([
-    { name: "John", age: 30 },
-    { name: "Jane", age: 25 },
-  ]);
-};
--- backend/api/users.ts
+++
@@ -1,6 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import { getUsers } from "./services/user";
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return getUsers(req, res);
-}
